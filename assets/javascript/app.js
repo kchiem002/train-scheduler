73de98c68fbe318a727f9c24371672a502f41b7a
Kathy Chiem <kchie002@gmail.com>
2019-03-25 22:27:19 -0700

    completed db push and pull, need to add times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,14 +8,14 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  let db = firebase.firestore()
-  let trainName
-  let destination
-  let frequency
+let db = firebase.firestore()
+let trainName
+let destination
+let frequency
 
-  document.querySelector('#submit').addEventListener('click', e => {
+document.querySelector('#submit').addEventListener('click', e => {
     e.preventDefault()
-    let id = db.collection('train-scheduler').doc().id
+    let id = db.collection('train').doc().id
     trainName = document.querySelector('#input-train-name').value
     destination = document.querySelector('#input-destination').value
     time = document.querySelector('#input-train-time').value
@@ -31,18 +31,21 @@ var config = {
     time = document.querySelector('#input-train-time').value = ''
     frequency = document.querySelector('#input-frequency').value = ''
 })
-db.collection('train-scheduler').onSnapshot(({ docs }) => {
-    document.querySelector('#test').innerHTML = ''
-    docs.forEach(doc => {
+
+db.collection('train').onSnapshot(snap => {
+    document.querySelector('#train-table').innerHTML = ''
+    snap.docs.forEach(doc => {
+
         let { trainName, destination, time, frequency } = doc.data()
-        let docElem = document.createElement('div')
+        let nextArrival = time
+        let docElem = document.createElement('tr')
         docElem.innerHTML = `
-            <p>${trainName}</p>
-            <p>${destination}</p>
-            <p>${time}</p>
-            <p>${frequency}</p>
+            <td>${trainName}</td>
+            <td>${destination}</td>
+            <td>${frequency} Minutes</td>
+            <td>${nextArrival} A.M.</td>
             `
-        document.querySelector('#test').append(docElem)
+        document.querySelector('#train-table').appendChild(docElem)
         console.log(docElem)
     })
 })
